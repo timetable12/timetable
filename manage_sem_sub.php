4ae9632726fb11f52c741cc002c604554d6f61bb
timetable12 <37617849+timetable12@users.noreply.github.com>
2018-04-12 05:43:03 -0700

    Add files via upload

--- manage_sem_sub.php
+++ manage_sem_sub.php
@@ -17,8 +17,8 @@ if(isset($_POST['update']))
 	$id=$_POST['I1'];
 	$name=$_POST['F1'];
 	$lect=$_POST['L1'];
-   $res= mysqli_query($conn,"UPDATE fac_load SET faculty_name='$name',L_count='$lect' 
-   	WHERE fac_id='$id'");
+   $res= mysqli_query($conn,"UPDATE semester_sub SET subject_id='$id',semester_name='$name',semester='$lect' 
+   	WHERE subject_id='$id'");
 }
 ?>
 <!DOCTYPE html>
@@ -26,7 +26,7 @@ if(isset($_POST['update']))
 <head>
 	<title></title>
 	<style type="text/css">
-		#fac_table,#text_box,#add,#rem_table{
+		#sem_table,#text_box,#add,#rem_table{
 		display :none;	
 		}
 	</style>
@@ -39,7 +39,7 @@ if(isset($_POST['update']))
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script>
 	function updateFn(){
-		document.getElementById("fac_table").style.display ="inline";
+		document.getElementById("sem_table").style.display ="inline";
 	    }
    function addFn(){
 		document.getElementById("add").style.display ="inline";
@@ -57,7 +57,7 @@ if(isset($_POST['update']))
      }
 
 function removeFn(){
-		document.getElementById("rem_table").style.display ="inline";
+		document.getElementById("remove_sub").style.display ="inline";
 	}
 function r1(myid){//getting values for deletion
     $.ajax({
@@ -70,25 +70,25 @@ function r1(myid){//getting values for deletion
     });
 }
 </script>  
- <div id="fac_table">
+ <div id="sem_table">
 <?php
-$result = mysqli_query($conn,"SELECT * FROM fac_load");
+$result = mysqli_query($conn,"SELECT * FROM semester_sub");
 $r1 =$result->num_rows;
 echo "<table border='1'>
 <tr>
-<th>Faculty ID</th>
-<th>Faculty Name</th>
-<th>Lectures Allocated</th>
+<th>Subject ID</th>
+<th>Subject Name</th>
+<th>Semester</th>
 <th>Edit</th>
 </tr>";
 for($i=1;$i<=$r1;$i++)
 {
  $row = mysqli_fetch_array($result);
  echo "<tr>"; 
- echo "<td>" . $row['fac_id'] . "</td>";
- echo "<td>" . $row['faculty_name'] . "</td>";
- echo "<td>" . $row['L_count'] . "</td>";
- echo "<td><button id=\"".$i ."\" onclick=f1(".$row['fac_id'].",'". $row['faculty_name']."',". $row['L_count'].")>edit</button></td><br>";
+ echo "<td>" . $row['subject_id'] . "</td>";
+ echo "<td>" . $row['subject_name'] . "</td>";
+ echo "<td>" . $row['semester'] . "</td>";
+ echo "<td><button id=\"".$i ."\" onclick=s1(".$row['subject_id'].",'". $row['subject_name']."',". $row['semester'].")>edit</button></td><br>";
 }
  
 echo "</tr>";
@@ -98,11 +98,11 @@ echo "</table>";
 
 <script type="text/javascript">
 
-	function f1(myid,name,Lectures){//getting values for updation
+	function s1(myid,name,sem){//getting values for updation
     document.getElementById("text_box").style.display ="inline";
-    document.getElementById("id").value =myid;
-    document.getElementById("name").value =name;
-    document.getElementById("lect").value =Lectures;
+    document.getElementById("sid").value =myid;
+    document.getElementById("sname").value =name;
+    document.getElementById("sem").value =sem;
 	}
 
 </script>
@@ -112,11 +112,11 @@ echo "</table>";
 <div id="rem_table">
 <?php
 
-$result = mysqli_query($conn,"SELECT * FROM fac_load");
+$result = mysqli_query($conn,"SELECT * FROM semester_sub");
 $r1 =$result->num_rows;
 echo "<table border='1'>
 <tr>
-<th>Faculty ID</th>
+<th>Semester ID</th>
 <th>Faculty Name</th>
 <th>Lectures Allocated</th>
 <th>Remove</th>
@@ -125,10 +125,10 @@ for($i=1;$i<=$r1;$i++)
 {
  $row = mysqli_fetch_array($result);
  echo "<tr>"; 
- echo "<td>" . $row['fac_id'] . "</td>";
- echo "<td>" . $row['faculty_name'] . "</td>";
- echo "<td>" . $row['L_count'] . "</td>";
- echo "<td><button id=\"".$i ."\" onclick=r1(".$row['fac_id'].")>remove</button></td><br>";
+ echo "<td>" . $row['subject_id'] . "</td>";
+ echo "<td>" . $row['semester_name'] . "</td>";
+ echo "<td>" . $row['semester'] . "</td>";
+ echo "<td><button id=\"".$i ."\" onclick=rSemSub(".$row['subject_id'].")>remove</button></td><br>";
 }
 echo "</tr>";
 echo "</table>";
@@ -136,15 +136,15 @@ echo "</table>";
 </div>
 <div id="text_box"><!--table for updation-->
 	<form action="" method="POST">
- <input type="text" name="I1" id="id" hidden><br><br>
-Faculty Name: <input type="text" name="F1" id="name"><br><br>
-No. of Lectures: <input type="text" name="L1" id="lect"><br><br>
+ Subject ID<input type="text" name="I1" id="sid"><br><br>
+Subject Name: <input type="text" name="F1" id="sname"><br><br>
+Semster: <input type="text" name="L1" id="sem"><br><br>
 <input type="submit" name="update" value="UPDATE">
 </form>
 </div>
 <div id="add">
 	<form>
-		Enter numbers of faculties to added :<input type="text" name="" id="textfac"><br><br>
+		Enter numbers of subjects to added :<input type="text" name="" id="textfac"><br><br>
 		<button onclick="addFac()">add</button>
 	</form>
 </div>
